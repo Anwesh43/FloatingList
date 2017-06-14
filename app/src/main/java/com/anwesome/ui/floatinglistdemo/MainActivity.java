d7018e08521260e2268ec652e2fbe339621bce4f
Anwesh43 <anweshthecool0@gmail.com>
2017-06-15 02:56:50 +0530

    Using OnItemSelectedListener in app

--- app/src/main/java/com/anwesome/ui/floatinglistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/floatinglistdemo/MainActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.anwesome.ui.floatinglist.FloatingList;
+import com.anwesome.ui.floatinglist.FloatingListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String options[] = {"hello","add","del","subtract"};
+        String options[] = {"addmore","delmore","nomore","subtract"};
         final FloatingList floatingList = new FloatingList(this);
         for(int i=0;i<options.length;i++) {
             floatingList.addItem(options[i]);
         }
+        floatingList.setOnItemSelectedListener(new FloatingListView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(String item) {
+                Toast.makeText(MainActivity.this, String.format("%s selected",item), Toast.LENGTH_SHORT).show();
+            }
+        });
         floatingList.addToParent();
         Button showBtn = (Button)findViewById(R.id.show_list);
         showBtn.setOnClickListener(new View.OnClickListener() {
